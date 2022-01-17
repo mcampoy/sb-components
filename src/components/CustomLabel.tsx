c84d038e4ba6f2fa13b54013a171107dbf0b5884
Matías Campoy <matiascampoy@gmail.com>
2022-01-17 17:16:36 -0300

    first commit

---
+++ src/components/CustomLabel.tsx
@@ -0,0 +1,42 @@
+import './CustomLabel.css';
+
+export interface CustomLabelProps {
+  /**
+   * Este es el mensaje que se mostrará en la etiqueta
+   */
+  label: string;
+  /**
+   * Tamaño de la etiqueta. Por defecto es "normal"
+   */
+  size: 'normal'|'h1'|'h2'|'h3';
+  /**
+   * Capitalizar toda la etiqueta
+   */
+  allCaps?: boolean;
+  /**
+   * Colores básicos de la fuente
+   */
+  color?: 'primary'|'secondary'|'tertiary';
+
+  /**
+   * Color personalizados de la fuente
+   */
+   customFontColor?: string;
+};
+
+export const CustomLabel = ({
+   allCaps  = false,
+   color    = 'primary',
+   label     = 'No Label',
+   size     = 'normal',
+   customFontColor
+  }: CustomLabelProps ) => {
+  return (
+    <span
+      className={ `label ${ size } text-${ color }` }
+      style={{color: customFontColor}}
+    >
+      {!allCaps ? label : label.toUpperCase()}
+    </span>
+  )
+};
---
+++ src/stories/components/CustomLabel.stories.tsx
@@ -0,0 +1,42 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { CustomLabel } from "../../components/CustomLabel";
+
+export default {
+  title: 'UI/CustomLabel',
+  component: CustomLabel,
+  argTypes: {
+    color: { control: 'select' },
+    size: { control: 'select' },
+    customFontColor: { control: 'color' }
+  }
+} as ComponentMeta<typeof CustomLabel>
+
+const Template: ComponentStory<typeof CustomLabel> = (args) => {
+  return <CustomLabel {...args} />;
+};
+
+export const Basic = Template.bind({});
+Basic.args = {
+  size: 'normal',
+  allCaps: false
+}
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+  size: 'normal',
+  allCaps: true
+}
+export const Secondary = Template.bind({});
+Secondary.args = {
+  size: 'normal',
+  color: 'secondary'
+}
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  size: 'normal',
+  color: 'tertiary'
+}
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = {
+  // customFontColor: '#5517ac',  
+  size: 'h1'
+}
